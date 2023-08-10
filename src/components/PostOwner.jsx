af9f06aa48b84ef8ed49d36eabe495128c93f48b
orlyomyim00e <yinpiao_wongtrakunme@cmu.ac.th>
2023-08-10 15:37:39 +0700

    commit6

--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -5,16 +5,28 @@ export const PostOwner = ({
   likeNum, 
 }) => {
   return (
-    <div className="d-flex gap-2 my-2 ps-5">
-    <img src={userImagePath} width="48" height="48" className="rounded-circle" style={{ objectFit: "cover" }} />
-    <div className="rounded rounded-3 p-2" style={{ backgroundColor: "#E5E7EB" }}>
-    <span className="fw-semibold">{username}</span> <br />
-    <span>{replyText}</span>
-    <div className="d-flex align-items-center gap-1">
-    <img src="/like.svg" width={20} alt="like icon" />
-    <span className="text-muted">{likeNum} คน</span>
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/Me.jpg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5">
+          {name} {studentID}
+        </span>
+      </div>
+
+      <span>{detail}</span>
+
+      <div className="d-flex align-items-center gap-1">
+        {likeNum > 0 && <img src="/like.svg" width={20}></img>}
+        {likeNum > 0 && <span className="text-muted">{likeNum} คน</span>}
+      </div>
+      <hr className="m-0 border" />
     </div>
-    </div>
-    </div>
-    );
-    };
+  );
+};
+   
--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,16 +1,27 @@
 export const Reply = ({ userImagePath, username, replyText, likeNum }) => {
   return (
-    <div className="d-flex gap-2 my-2 ps-5">
-    <img src={userImagePath} width="48" height="48" className="rounded-circle" style={{ objectFit: "cover" }} />
-    <div className="rounded rounded-3 p-2" style={{ backgroundColor: "#E5E7EB" }}>
-    <span className="fw-semibold">{username}</span>
-    <br />
-    <span>{replyText}</span>
-    <div className="d-flex align-items-center gap-1">
-    <img src="/like.svg" width={20} alt="Like" />
-    <span className="text-muted">{likeNum} คน</span>
+    <div>
+      <div className="d-flex gap-2 my-2 ps-5">
+        <img
+          src={userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{username}</span>
+          <br />
+          <span>{replyText}</span>
+          <div className="d-flex align-items-center gap-1">
+            {likeNum > 0 && <img src="/like.svg" width={20}></img>}
+            {likeNum > 0 && <span className="text-muted">{likeNum} คน</span>}
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-    );
-    };
+  );
+};
