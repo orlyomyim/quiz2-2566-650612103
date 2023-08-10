b0108721ffdec6f3fe68765877a944896073ad11
orlyomyim00e <yinpiao_wongtrakunme@cmu.ac.th>
2023-08-10 15:28:38 +0700

    commit5

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -6,16 +6,20 @@ export const Comment = ({
   replies,
 }) => {
   return (
+    <div>
     <div className="d-flex gap-2 my-2">
     <img src={userImagePath} width="48" height="48" className="rounded-circle" style={{ objectFit: "cover" }} />
     <div className="rounded rounded-3 p-2" style={{ backgroundColor: "#E5E7EB" }}>
-    <span className="fw-semibold">{username}</span> <br />
+    <span className="fw-semibold">{username}</span>
+    <br />
     <span>{commentText}</span>
     <div className="d-flex align-items-center gap-1">
-    <img src="/like.svg" width={20} alt="like icon" />
+    <img src="/like.svg" width={20} alt="Like" />
     <span className="text-muted">{likeNum} คน</span>
     </div>
-    {/* สร้าง Reply Component ในการวนลูปแสดงรีพลาย */}
+    </div>
+    </div>
+    {/* map-loop render Reply component here */}
     {replies.map((reply, index) => (
     <Reply
     key={index}
@@ -26,6 +30,5 @@ export const Comment = ({
     />
     ))}
     </div>
-    </div>
     );
-};
+  };
